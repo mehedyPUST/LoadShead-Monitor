73c911c5b701ccbd779e684732d6bb3bf26f8b30
mehedyPUST <mehedy.pust@gmail.com>
2026-08-05 23:21:49 +0600

    header separate

--- src/components/common/CompanyHeader.jsx
+++ src/components/common/CompanyHeader.jsx
@@ -7,7 +7,7 @@ export default function CompanyHeader() {
                 <div className="flex flex-col items-center justify-center gap-2">
                     {/* Logo */}
                     <img
-                        src="/logo.png"
+                        src="/public/logo.png"
                         alt="WZPDCL Logo"
                         className="h-14 w-auto md:h-16 object-contain brightness-0 invert"
                         onError={(e) => {
